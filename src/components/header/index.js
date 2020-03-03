c399e145eb5aa2f8352b57744f6b2b81a94e3816
QuocTung <quoctung.haui@gmail.com>
2020-03-03 11:46:26 +0700

    change icon commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,10 @@ import Menu from '../common/menu'
 import Booking from '../common/booking'
 import  './css.css'
 function Hook () {
+  const backgr = require('../../static/images/home_banner.jpg')
       return (
           <Fragment>
-            <div className="wrap-header">
+            <div className="wrap-header"  style={{backgroundImage: `url('${backgr}')`}}>
               
               <Menu/>
               <Booking/>
